ba762a3192ad1039420f88a1dcd6cdb83f1fe4dc
andrew82106 <qq281512383@163.com>
2022-03-11 15:14:08 +0800

    第一次上传

---
+++ Main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def show_Table():
+    text = """
+    ------------------English Revision-----------------------
+    ---------------------------------------------------------
+    >>>指令模式：
+    >>>ADD WORD：向词库里面添加单词
+    >>>TEST：开始测试
+    """
+    print(text)
+
+
+def main():
+    show_Table()
+    return 0
+
+
+main()
